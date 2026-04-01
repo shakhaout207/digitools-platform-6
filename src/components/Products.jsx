b173723ed8d3decabab5a6a6d80666520bc6c2f7
shakhaouth207 <shakhaouthossain077@gmail.com>
2026-04-01 16:25:11 +0600

    product update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,7 +2,7 @@ import ProductCard from "./ProductCard";
 
 const Products = ({ products, addToCart }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-7">
       {products.map((product) => (
         <ProductCard
           key={product.id}
